77b4d46fad50ac555ce7def39b304f2a596f719a
eemmtt <52972088+eemmtt@users.noreply.github.com>
2023-02-28 11:13:54 -0500

    is this thing on

--- app.py
+++ app.py
@@ -7,6 +7,16 @@ from slack_bolt.adapter.socket_mode import SocketModeHandler #for socket hosting
 
 app = App(token=os.environ["SLACK_BOT_TOKEN"])
 
+@app.event("app_home_opened")
+def update_home_tab(client, event, logger):
+  try:
+    client.views_publish(
+      user_id=event["user"],
+      view=compose_home()
+    )
+  except Exception as e:
+    logger.error(f"Error publishing home tab: {e}")
+
 if __name__ == '__main__':
     print("Hello Darling")
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start() 
--- views.py
+++ views.py
@@ -1,1 +1,19 @@
 #slack ui views
+
+def compose_home():
+    view = {
+	"blocks": [
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "Welcome person"
+			},
+			"accessory": {
+				"type": "image",
+				"image_url": "https://pbs.twimg.com/profile_images/625633822235693056/lNGUneLX_400x400.jpg",
+				"alt_text": "it's you"
+			}
+		}
+	]
+}
