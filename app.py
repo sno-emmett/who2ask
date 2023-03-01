80b7a520e56609cbc06147045ee9df813ba0d860
eemmtt <52972088+eemmtt@users.noreply.github.com>
2023-03-01 14:25:44 -0500

    mockup search in home tab

--- app.py
+++ app.py
@@ -6,16 +6,52 @@ from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler #for socket hosting
 
 app = App(token=os.environ["SLACK_BOT_TOKEN"])
+homeviews = {}
+# "/who [topic]" will return a list of people with relevant knowledge of the topic.
+# "/what [name]" will return a list of topics the named person has volunteered. 
 
 @app.event("app_home_opened")
-def update_home_tab(client, event, logger):
+def app_home_opened(client, event, logger):
+  user_id = event["user"]
+  user_name = ""
+  user_image = ""
+  
+  # Get user info (eventually we'll cache this)
   try:
-    client.views_publish(
-      user_id=event["user"],
-      view=compose_home()
+    response = client.users_profile_get(user=user_id)
+    user_name = response["profile"]["real_name_normalized"]
+    user_image = response["profile"]["image_72"]
+  except Exception as e:
+    logger.error("Error: {}".format(e))
+  
+  # Compose the home tab view
+  # check if user had open search results
+  if user_id not in homeviews:
+    homeviews[user_id] = compose_home(user_name, user_image)
+  
+  try:
+    app.client.views_publish(
+      user_id=user_id,
+      view=homeviews[user_id]
     )
   except Exception as e:
     logger.error(f"Error publishing home tab: {e}")
+
+@app.action("input_search")
+def handle_search(ack, body, logger):
+    ack()
+    query = body['actions'][0]['value']
+    user_id = body['user']['id']
+    
+    # update homeviews[user_id] with search results so that if user clicks away, they can return to their search results
+    homeviews[user_id] = compose_search_results(query)
+    try:
+      app.client.views_publish(
+        user_id=user_id,
+        view=homeviews[user_id]
+      )
+    except Exception as e:
+      logger.error(f"Error publishing home tab: {e}")
 
 if __name__ == '__main__':
     print("Hello Darling")
--- views.py
+++ views.py
@@ -1,19 +1,101 @@
 #slack ui views
 
-def compose_home():
+def compose_home(user_name, user_image):
     view = {
+    "type": "home",
 	"blocks": [
-		{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": "Welcome person"
+        {
+			"dispatch_action": True,
+			"type": "input",
+			"element": {
+				"type": "plain_text_input",
+				"action_id": "input_search",
+				"placeholder": {
+					"type": "plain_text",
+					"text": "[Topic Name]"
+				}
 			},
-			"accessory": {
-				"type": "image",
-				"image_url": "https://pbs.twimg.com/profile_images/625633822235693056/lNGUneLX_400x400.jpg",
-				"alt_text": "it's you"
+			"label": {
+				"type": "plain_text",
+				"text": ":mag: Search ",
+				"emoji": True
 			}
 		}
 	]
-}
+    }
+    return view
+
+def compose_search_results(query):
+    view = {
+        "type": "home",
+        "blocks": [
+            {
+			"dispatch_action": True,
+			"type": "input",
+			"element": {
+				"type": "plain_text_input",
+				"action_id": "input_search",
+				"placeholder": {
+					"type": "plain_text",
+					"text": "[Topic Name]"
+				}
+			},
+			"label": {
+				"type": "plain_text",
+				"text": ":mag: Search ",
+				"emoji": True
+			}
+		    },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": f"Search results for *{query}:*"
+                }
+            },
+            {
+                "type": "divider"
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "*nothing*\nI've been folding gliders for years! Ask me anything.\n-<https://acmeco.slack.com/team/U1H63D8SZ|Little Cat>"
+                },
+                "accessory": {
+                    "type": "image",
+                    "image_url": "https://pbs.twimg.com/profile_images/625633822235693056/lNGUneLX_400x400.jpg",
+                    "alt_text": "username"
+                }
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "*Paper Airplanes*\nI've been folding gliders for years! Ask me anything.\n-<https://acmeco.slack.com/team/U1H63D8SZ|Little Cat>"
+                },
+                "accessory": {
+                    "type": "image",
+                    "image_url": "https://pbs.twimg.com/profile_images/625633822235693056/lNGUneLX_400x400.jpg",
+                    "alt_text": "username"
+                }
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "*Paper Airplanes*\nI've been folding gliders for years! Ask me anything.\n-<https://acmeco.slack.com/team/U1H63D8SZ|Little Cat>"
+                },
+                "accessory": {
+                    "type": "image",
+                    "image_url": "https://pbs.twimg.com/profile_images/625633822235693056/lNGUneLX_400x400.jpg",
+                    "alt_text": "username"
+                }
+            },
+            {
+                "type": "divider"
+            }
+            
+        ]
+    }
+    return view
